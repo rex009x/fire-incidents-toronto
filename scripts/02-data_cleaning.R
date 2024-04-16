f14085ad8ebcf084200d18f902e931a151dfa601
Jimmy Luc <jimmy.luc@mail.utoronto.ca>
2024-04-16 16:56:48 -0400

    update data cleaning script and data files

--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -18,7 +18,7 @@ raw_fire_incidents_data <-
     file=here("data/raw_data/raw_fire_incidents.csv")
   )
 
-# clean the names and select column of interest
+# clean the names and select column of interest, 29425 entries
 cleaned_fire_incidents_data <-
   clean_names(raw_fire_incidents_data) |>
   select(
@@ -37,7 +37,21 @@ cleaned_fire_incidents_data <-
     tfs_arrival_time
   )
 
-# TODO clean selected variables of interest (omit NA values)
+# clean selected variables of interest (omit NA values), 15628 entries
+cleaned_fire_incidents_data <- cleaned_fire_incidents_data %>%
+  drop_na(area_of_origin,
+          estimated_dollar_loss,
+          latitude,
+          longitude,
+          method_of_fire_control,
+          number_of_responding_personnel,
+          possible_cause,
+          property_use,
+          smoke_alarm_at_fire_origin,
+          smoke_alarm_at_fire_origin_alarm_failure,
+          smoke_alarm_at_fire_origin_alarm_type,
+          tfs_alarm_time,
+          tfs_arrival_time)
 
 #### Save data ####
 # write cleaned data as csv
