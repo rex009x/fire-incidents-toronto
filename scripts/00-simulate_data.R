18848a13bd5a22d40466d7d1da33b973fd1f9f2e
Jimmy Luc <jimmy.luc@mail.utoronto.ca>
2024-04-16 16:09:20 -0400

    update data simulation script

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -10,6 +10,26 @@
 library(tidyverse)
 
 #### Simulate data ####
-# TODO set seed for simulation
+# set seed for simulation
+set.seed(905)
 
-# TODO create a simulation dataset with variables of interest following the proper distribution
+# create a simulation dataset with variables of interest following the proper distribution
+property_uses <- c("condo", "apartment", "single_detached", "warehouse") %>%
+  rep(each = 5, length.out = 20)
+
+estimated_dollar_loss <- rnorm(property_uses, 25000, 12500) %>%
+  round() %>%
+  pmax(0)
+
+estimated_loss_property_use <-
+  tibble(
+    property_use = property_uses,
+    estimated_dollar_loss = estimated_dollar_loss
+  )
+
+
+#### Test data ####
+## test data types##
+# methods used: https://stackoverflow.com/a/29591221
+all(sapply(estimated_loss_property_use$property_use, is.character))
+all(sapply(estimated_loss_property_use$estimated_dollar_loss, is.numeric))
