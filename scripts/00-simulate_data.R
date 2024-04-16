043596dfeb60ccb27f96f342822a7ac6a4a1a7cc
Jimmy Luc <jimmy.luc@mail.utoronto.ca>
2024-04-16 16:19:10 -0400

    update simulation data

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -13,7 +13,7 @@ library(tidyverse)
 # set seed for simulation
 set.seed(905)
 
-# create a simulation dataset with variables of interest following the proper distribution
+# simulate estimated dollar loss and property use data
 property_uses <- c("condo", "apartment", "single_detached", "warehouse") %>%
   rep(each = 5, length.out = 20)
 
@@ -27,9 +27,26 @@ estimated_loss_property_use <-
     estimated_dollar_loss = estimated_dollar_loss
   )
 
+# simulate estimated dollar loss and area of origin data
+areas_of_origin <- c("kitchen", "trash", "office", "porch") %>%
+  rep(each = 5, length.out = 20)
+
+estimated_dollar_loss <- rnorm(areas_of_origin, 25000, 12500) %>%
+  round() %>%
+  pmax(0)
+
+estimated_loss_area_of_origin <-
+  tibble(
+    area_of_origin = areas_of_origin,
+    estimated_dollar_loss = estimated_dollar_loss
+  )
+
 
 #### Test data ####
 ## test data types##
 # methods used: https://stackoverflow.com/a/29591221
 all(sapply(estimated_loss_property_use$property_use, is.character))
 all(sapply(estimated_loss_property_use$estimated_dollar_loss, is.numeric))
+
+all(sapply(estimated_loss_area_of_origin$area_of_origin, is.character))
+all(sapply(estimated_loss_area_of_origin$estimated_dollar_loss, is.numeric))
